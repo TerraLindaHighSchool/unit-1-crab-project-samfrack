6256afb7c55dd3aa041038b050690e0f83f07fe8
samfrack <89489598+samfrack@users.noreply.github.com>
2021-08-26 17:45:03 -0500

    Activity 2.2

--- Crab.java
+++ Crab.java
@@ -2,13 +2,35 @@ import greenfoot.*;
 
 /**
  * This class defines a crab. Crabs live on the beach.
+ * @author Sam Frack
+ * @version 8/26/2021
  */
 public class Crab extends Actor
 {
+    //This method repeats the following actions
     public void act()
     {
         // Add your action code here
     }
-}
+    
+    // Moves the Crab
+    
+    // Turns the Crab at the edge
+    private void turnAtEdge()
+    {
+        
+    }
+    // Checks for user key presses so user can turn the Crab
+    private void checkKeyPress()
+    {
+
+    }
+        
+    //Checks for collisions with other objects
+    private void onCollision()
+    {
+        
+    }
+
 
 
