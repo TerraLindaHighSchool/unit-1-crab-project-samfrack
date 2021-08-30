66b7dd113270ae8ae2e45a4630af1f7c6edb815c
samfrack <89489598+samfrack@users.noreply.github.com>
2021-08-30 17:46:11 -0500

    Completed Activity 2

--- Crab.java
+++ Crab.java
@@ -10,16 +10,21 @@ public class Crab extends Actor
     //This method repeats the following actions
     public void act()
     {
-        // Add your action code here
+        move(3);
+        turnAtEdge();
+        checkKeyPress();
+        onCollision();
     }
-    
-    // Moves the Crab
     
     // Turns the Crab at the edge
     private void turnAtEdge()
     {
-        
+        if(isAtEdge())
+        {
+            turn(50);
+        }
     }
+    
     // Checks for user key presses so user can turn the Crab
     private void checkKeyPress()
     {
@@ -33,4 +38,4 @@ public class Crab extends Actor
     }
 
 
-
+}
